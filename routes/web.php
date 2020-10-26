bb88e299ceaf8cde35409931e9c85a2a23a64e65
chengkai <10863105@gm.nfu.edu.tw>
2020-10-26 16:23:17 +0800

    1.policy.blade.php added policy page

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 //社群登入路由=================================================
 Route::get('/login/{provider}', 'Auth\LoginController@redirectToProvider');
 Route::get('/login/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+
+Route::get('/policy', function () {
+    return view('policy');
+});
